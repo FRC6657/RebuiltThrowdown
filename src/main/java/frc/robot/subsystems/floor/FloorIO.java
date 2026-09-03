e5cb66060effa6a36d885e2ee5db5ff78a2e284c
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-09-03 15:47:45 -0700

    Add FloorIO Sim & modify existing FloorIO

--- src/main/java/frc/robot/subsystems/floor/FloorIO.java
+++ src/main/java/frc/robot/subsystems/floor/FloorIO.java
@@ -1,17 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.floor;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class FloorIO extends SubsystemBase {
-  /** Creates a new FloorIO. */
-  public FloorIO() {}
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+public interface FloorIO {
+    
 }
---
+++ src/main/java/frc/robot/subsystems/floor/FloorIO_Sim.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.floor;
+
+public class FloorIO_Sim implements FloorIO {
+    
+}
