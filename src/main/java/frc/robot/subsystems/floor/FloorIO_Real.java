5cd00181f62ed8e011a52e3afab76ebf4e0ebb92
Anmol-Cheema <anmol.cheema@uphsconnect.org>
2026-09-03 16:47:47 -0700

    Worked on Floor FloorIO FloorIOReal and constants

--- src/main/java/frc/robot/subsystems/floor/FloorIO_Real.java
+++ src/main/java/frc/robot/subsystems/floor/FloorIO_Real.java
@@ -1,5 +1,58 @@
-package frc.robot.subsystems.floor;
+package frc.robot.subsystems.indexer.floor;
+
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import frc.robot.GlobalConstants;
+
 
 public class FloorIO_Real implements FloorIO {
-    
+
+  TalonFX motorOne = new TalonFX(GlobalConstants.CAN.Floor.id);
+  TalonFX motorTwo = new TalonFX(GlobalConstants.CAN.Ceiling_Wall.id);
+  VoltageOut setpoint = new VoltageOut(0);
+
+  public FloorIO_Real() {
+
+    motorOne.getConfigurator().apply(FloorConstants.CONFIG);
+    motorTwo.getConfigurator().apply(FloorConstants.CONFIG);
+
+    motorTwo.setControl(new Follower(GlobalConstants.CAN.Floor.id, MotorAlignmentValue.Opposed));
+
+    var motorOneTemp = motorOne.getDeviceTemp();
+    var motorOneVoltage = motorOne.getMotorVoltage();
+    var motorOneStatorCurrent = motorOne.getStatorCurrent();
+    var motorTwoTemp = motorTwo.getDeviceTemp();
+    var motorTwoVoltage = motorTwo.getMotorVoltage();
+    var motorTwoStatorCurrent = motorTwo.getStatorCurrent();
+
+    motorOneTemp.setUpdateFrequency(GlobalConstants.mainLoopFrequency / 4);
+    motorOneVoltage.setUpdateFrequency(GlobalConstants.mainLoopFrequency);
+    motorOneStatorCurrent.setUpdateFrequency(GlobalConstants.mainLoopFrequency);
+    motorTwoTemp.setUpdateFrequency(GlobalConstants.mainLoopFrequency / 4);
+    motorTwoVoltage.setUpdateFrequency(GlobalConstants.mainLoopFrequency);
+    motorTwoStatorCurrent.setUpdateFrequency(GlobalConstants.mainLoopFrequency);
+
+    motorOne.optimizeBusUtilization();
+    motorTwo.optimizeBusUtilization();
+  }
+
+  @Override
+  public void updateInputs(FloorIOInputs inputs) {
+
+    motorOne.setControl(setpoint);
+
+    inputs.motorOneTemp = motorOne.getDeviceTemp().getValueAsDouble();
+    inputs.motorOneStatorCurrent = motorOne.getStatorCurrent().getValueAsDouble();
+    inputs.motorOneVoltage = motorOne.getMotorVoltage().getValueAsDouble();
+    inputs.motorTwoTemp = motorTwo.getDeviceTemp().getValueAsDouble();
+    inputs.motorTwoStatorCurrent = motorTwo.getStatorCurrent().getValueAsDouble();
+    inputs.motorTwoVoltage = motorTwo.getMotorVoltage().getValueAsDouble();
+  }
+
+  @Override
+  public void changeSetpoint(double setpoint) {
+    this.setpoint.Output = setpoint;
+  }
 }
