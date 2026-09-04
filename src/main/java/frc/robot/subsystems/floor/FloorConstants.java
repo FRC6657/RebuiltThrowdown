52eb754a0897348adb189125098ba18b0c1c2ab1
Anmol-Cheema <anmol.cheema@uphsconnect.org>
2026-09-03 17:21:18 -0700

    Developed Floor and components

--- src/main/java/frc/robot/subsystems/floor/FloorConstants.java
+++ src/main/java/frc/robot/subsystems/floor/FloorConstants.java
@@ -1,10 +1,34 @@
-package frc.robot.subsystems.floor;
+package frc.robot.subsystems.indexer.floor;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.system.plant.DCMotor;
 
-public class FloorConstants {
 
+  public static final double Off = 0.0; 
+  public static final double FORWARD = 12; // 6V forward (move game pieces toward tunnel)
+  public static final double REVERSE = -6; // 6V reverse (eject game pieces)
+
+  public final double voltage = 0.0;
+
+  public static final DCMotor MOTOR = DCMotor.getFalcon500(1);
+  public static final double GEAR_RATIO = 32d / 11d; 
+
+  public static final TalonFXConfiguration CONFIG =
+      new TalonFXConfiguration()
+          .withMotorOutput(
+              new MotorOutputConfigs()
+                  .withInverted(InvertedValue.CounterClockwise_Positive)
+                  .withNeutralMode(NeutralModeValue.Coast))
+          .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(GEAR_RATIO))
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withSupplyCurrentLimit(40)
+                  .withStatorCurrentLimit(40)
+                  .withSupplyCurrentLimitEnable(true)
+                  .withStatorCurrentLimitEnable(true));
 }
