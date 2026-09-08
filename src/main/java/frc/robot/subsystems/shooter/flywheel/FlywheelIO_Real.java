c3ec7f8d40ca75ed1121eec686c4a5c860da89cc
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-09-08 16:31:32 -0700

    Remake shooter classes

---
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIO_Real.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.shooter.flywheel;
+
+public class FlywheelIO_Real implements FlywheelIO {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/pivot/PivotIO_Real.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.shooter.pivot;
+
+public class PivotIO_Real implements PivotIO {
+    
+}
