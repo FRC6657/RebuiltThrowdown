d3d33b76ea5ba0b9d4c8080fb80a426a0d7f183d
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-09-10 16:09:05 -0700

    Make Superstructure

---
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import frc.robot.subsystems.drive.MAXSwerve;
+import frc.robot.subsystems.floor.Floor;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.flywheel.Flywheel;
+import frc.robot.subsystems.shooter.pivot.Pivot;
+
+/** The core class where all robot commands live. */
+public class Superstructure {
+  MAXSwerve drivebase;
+  Floor floor;
+  Intake intake;
+  Flywheel flywheel;
+  Pivot pivot;
+
+  public Superstructure(
+      MAXSwerve drivebase, Floor floor, Intake intake, Flywheel flywheel, Pivot pivot) {
+    this.drivebase = drivebase;
+    this.floor = floor;
+    this.intake = intake;
+    this.flywheel = flywheel;
+    this.pivot = pivot;
+
+    
+  }
+}
